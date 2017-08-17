895bfe26c71de716b8c77d9d4722bb4d256ac503
Deni Setiawan <deni.setiawan@inmagine.com>
2017-08-17 20:23:05 +0700

    add file upload plugin

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const fileUpload = require('express-fileupload');
 var app = express();
+
+// default options
+app.use(fileUpload());
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,6 +15,20 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.post('/upload', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('public/files/' + sampleFile.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
 });
 
 app.listen(app.get('port'), function() {
